057e6c5584d6481f6dcab021d4cfffe1d829ab1d
aqulzz <am.fadwa15@gmail.com>
2023-07-14 14:49:24 +0700

    chore: update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Wibusoft - Documentation',
   tagline: 'Simple Bot Hosting',
-  url: 'https://www.wibusoft.com',
+  url: 'https://docs.wibusoft.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
